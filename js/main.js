086993f8412746551c677ca8a749a8620573a241
CaffeinePower <yuto1001okuda@gmail.com>
2021-07-08 11:19:33 +0900

    タブをまたいだときに出力コードが変わらないバグ修正

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ function changeCategory() {
     if(openedCategory != null) {
         const settingsCategoryId = openedCategory.children[0].getAttribute("id");
         const allCategory = document.getElementsByClassName("accordion-body");
+        console.log(openedCategory);
 
         for (const key in allCategory) {
             allCategory[key].onchange = null;
@@ -28,10 +29,18 @@ function changeCategory() {
     }
 }
 
+function closeAccordion() {
+    const openedAccordion = document.getElementsByClassName("accordion-collapse collapse show");
+
+    for (const key in openedAccordion) {
+        openedAccordion[key].className = "accordion-collapse collapse";
+    }
+}
+
 //更新確認
 const observer = new MutationObserver(changeCategory);
 
-const target = document.getElementById("settingsList");
+const target = document.getElementById("settingsTabs");
 const obsConfig = {
     attributes: true,
     childList: true,
