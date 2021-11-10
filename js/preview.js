3d6897b1e61ab83fceedeabdfaa314a71ce6c795
Caffeine Power <yuto1001okuda@gmail.com>
2021-11-10 09:47:21 +0900

    バグ修正

--- js/preview.js
+++ js/preview.js
@@ -1,4 +1,4 @@
 function genPreview() {
     const previewDiv = document.getElementById("previewDiv");
-    previewDiv.innerHTML = "<h2>プレビュー</h2>" + document.getElementById("outputArea").textContent;
+    previewDiv.innerHTML = "<h2>プレビュー</h2><div  class='border rounded'>" + document.getElementById("outputArea").textContent + "</div>";
 }
