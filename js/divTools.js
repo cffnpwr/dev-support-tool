d70a1e8eecc2b0bae6c7eae06b2f9566aab4d486
AOI-POI <m19044@g.metro-cit.ac.jp>
2022-01-31 22:05:21 +0900

    padding, margin を実装

--- js/divTools.js
+++ js/divTools.js
@@ -3,7 +3,8 @@ window.addEventListener('DOMContentLoaded', codeGenerator, false);
 
 
 //値配列
-const vList = ["Auto", 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 20, 24, 28, 32, 36, 44, 48, 52, 56, 60, 64, 72, 80, 96]
+const sizeList = ["Auto", 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 20, 24, 28, 32, 36, 44, 48, 52, 56, 60, 64, 72, 80, 96]
+const spaceList = ["None", 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 20, 24, 28, 32, 36, 44, 48, 52, 56, 60, 64, 72, 80, 96]
 
 
 // Sizing (縦)
@@ -12,7 +13,7 @@ const divSizeHeightClass = "h-";
 let divSizeHeightCode = "";
 
 divSizeHeight.addEventListener('input', function () {
-    divSizeHeightCode = giveClass(vList[divSizeHeight.value], divSizeHeightClass);
+    divSizeHeightCode = giveClass(sizeList[divSizeHeight.value], divSizeHeightClass);
     document.getElementById("divSizeHeihgtValue").innerHTML = displaySize(divSizeHeight.value);
     codeGenerator();
 }, false);
@@ -23,8 +24,97 @@ const divSizeWidthClass = "&nbspw-";
 let divSizeWidthCode = "";
 
 divSizeWidth.addEventListener('input', function () {
-    divSizeWidthCode = giveClass(vList[divSizeWidth.value], divSizeWidthClass);
+    divSizeWidthCode = giveClass(sizeList[divSizeWidth.value], divSizeWidthClass);
     document.getElementById("divSizeWidthValue").innerHTML = displaySize(divSizeWidth.value);
+    codeGenerator();
+}, false);
+
+
+// Padding (上)
+const divSpacePaddingTop = document.getElementById("divSpacePaddingTop");
+const divSpacePaddingTopClass = "&nbsppt-";
+let divSpacePaddingTopCode = "";
+
+divSpacePaddingTop.addEventListener('input', function () {
+    divSpacePaddingTopCode = giveClass(spaceList[divSpacePaddingTop.value], divSpacePaddingTopClass);
+    document.getElementById("divSpacePaddingTopValue").innerHTML = displaySpace(divSpacePaddingTop.value);
+    codeGenerator();
+}, false);
+
+// Padding (下)
+const divSpacePaddingBottom = document.getElementById("divSpacePaddingBottom");
+const divSpacePaddingBottomClass = "&nbsppb-";
+let divSpacePaddingBottomCode = "";
+
+divSpacePaddingBottom.addEventListener('input', function () {
+    divSpacePaddingBottomCode = giveClass(spaceList[divSpacePaddingBottom.value], divSpacePaddingBottomClass);
+    document.getElementById("divSpacePaddingBottomValue").innerHTML = displaySpace(divSpacePaddingBottom.value);
+    codeGenerator();
+}, false);
+
+// Padding (右)
+const divSpacePaddingRight = document.getElementById("divSpacePaddingRight");
+const divSpacePaddingRightClass = "&nbsppr-";
+let divSpacePaddingRightCode = "";
+
+divSpacePaddingRight.addEventListener('input', function () {
+    divSpacePaddingRightCode = giveClass(spaceList[divSpacePaddingRight.value], divSpacePaddingRightClass);
+    document.getElementById("divSpacePaddingRightValue").innerHTML = displaySpace(divSpacePaddingRight.value);
+    codeGenerator();
+}, false);
+
+// Padding (左)
+const divSpacePaddingLeft = document.getElementById("divSpacePaddingLeft");
+const divSpacePaddingLeftClass = "&nbsppl-";
+let divSpacePaddingLeftCode = "";
+
+divSpacePaddingLeft.addEventListener('input', function () {
+    divSpacePaddingLeftCode = giveClass(spaceList[divSpacePaddingLeft.value], divSpacePaddingLeftClass);
+    document.getElementById("divSpacePaddingLeftValue").innerHTML = displaySpace(divSpacePaddingLeft.value);
+    codeGenerator();
+}, false);
+
+// Margin (上)
+const divSpaceMarginTop = document.getElementById("divSpaceMarginTop");
+const divSpaceMarginTopClass = "&nbspmt-";
+let divSpaceMarginTopCode = "";
+
+divSpaceMarginTop.addEventListener('input', function () {
+    divSpaceMarginTopCode = giveClass(spaceList[divSpaceMarginTop.value], divSpaceMarginTopClass);
+    document.getElementById("divSpaceMarginTopValue").innerHTML = displaySpace(divSpaceMarginTop.value);
+    codeGenerator();
+}, false);
+
+// Margin (下)
+const divSpaceMarginBottom = document.getElementById("divSpaceMarginBottom");
+const divSpaceMarginBottomClass = "&nbspmb-";
+let divSpaceMarginBottomCode = "";
+
+divSpaceMarginBottom.addEventListener('input', function () {
+    divSpaceMarginBottomCode = giveClass(spaceList[divSpaceMarginBottom.value], divSpaceMarginBottomClass);
+    document.getElementById("divSpaceMarginBottomValue").innerHTML = displaySpace(divSpaceMarginBottom.value);
+    codeGenerator();
+}, false);
+
+// Margin (右)
+const divSpaceMarginRight = document.getElementById("divSpaceMarginRight");
+const divSpaceMarginRightClass = "&nbspmr-";
+let divSpaceMarginRightCode = "";
+
+divSpaceMarginRight.addEventListener('input', function () {
+    divSpaceMarginRightCode = giveClass(spaceList[divSpaceMarginRight.value], divSpaceMarginRightClass);
+    document.getElementById("divSpaceMarginRightValue").innerHTML = displaySpace(divSpaceMarginRight.value);
+    codeGenerator();
+}, false);
+
+// Margin (左)
+const divSpaceMarginLeft = document.getElementById("divSpaceMarginLeft");
+const divSpaceMarginLeftClass = "&nbspml-";
+let divSpaceMarginLeftCode = "";
+
+divSpaceMarginLeft.addEventListener('input', function () {
+    divSpaceMarginLeftCode = giveClass(spaceList[divSpaceMarginBottom.value], divSpaceMarginLeftClass);
+    document.getElementById("divSpaceMarginLeftValue").innerHTML = displaySpace(divSpaceMarginLeft.value);
     codeGenerator();
 }, false);
 
@@ -41,18 +131,27 @@ function giveClass(parameter, className) {
 }
 
 
-// 値の表示
+// Sizing 値の表示
 function displaySize(parameter) {
-    if (vList[parameter] == 0) {
+    if (sizeList[parameter] == 0) {
         return "Auto";
     } else {
-        return vList[parameter];
+        return sizeList[parameter];
+    }
+}
+
+// Spacing 値の表示
+function displaySpace(parameter) {
+    if (spaceList[parameter] == 0) {
+        return "None";
+    } else {
+        return spaceList[parameter];
     }
 }
 
 
 // コード生成
 function codeGenerator() {
-    const codeGeneration = '<div class="' + divSizeHeightCode + divSizeWidthCode + '"></div>'
+    const codeGeneration = '<div class="' + divSizeHeightCode + divSizeWidthCode + divSpacePaddingTopCode + divSpacePaddingBottomCode + divSpacePaddingRightCode + divSpacePaddingLeftCode + divSpaceMarginTopCode + divSpaceMarginBottomCode + divSpaceMarginRightCode + divSpaceMarginLeftCode + '"></div>'
     document.getElementById("outputCode").innerHTML = codeGeneration;
 }
