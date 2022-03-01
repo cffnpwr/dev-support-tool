a15d21afe9745583cb608c4ecf7f9feab99b807d
Caffeine Power <yuto1001okuda@gmail.com>
2022-03-02 00:17:51 +0900

    [fix] コビーボタンの挙動を修正

--- js/copyClipboard.js
+++ js/copyClipboard.js
@@ -5,10 +5,11 @@
 function copyToClipboard(id) {
     const target = document.getElementById("codeBlock");
     navigator.clipboard.writeText(target.value);
-    const prevText = target.textContent;
-    document.getElementById(id, prevText).textContent = "コピーしました";
+    const button = document.getElementById(id);
+    const prevText = button.textContent;
+    button.textContent = "コピーしました";
 
-    setInterval(restoreButtonText, 3000, id);
+    setInterval(restoreButtonText, 3000, id, prevText);
 }
 
 function restoreButtonText(id, text) {
